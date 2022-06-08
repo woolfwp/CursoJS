a3fac72a4f0f38f8b1e73e752b0dd244ee833480
Sebastian Gomez <sebasgomez1099@gmail.com>
2022-06-08 11:58:51 -0500

    Se crean los primero metodos de la clase orden sistema de ventas

--- sistemaDeVentas/main.js
+++ sistemaDeVentas/main.js
@@ -1,27 +1,46 @@
-class Producto{
-    static contadorProductos = 0;
-    constructor(nombre, precio){
-        this._idproducto = ++Producto.contadorProductos;
-        this._nombre = nombre;
-        this._precio = precio;
-    }
-        get _idproducto() {
-        return this._idproducto;
-      }
-    
-      get nombre() {
-        return this._nombre;
-      }
-    
-      set nombre(nombre) {
-        this._nombre = nombre;
-      }
-    
-      get precio() {
-        return this._precio;
-      }
-    
-      set precio(precio) {
-        this._precio = precio;
-      }
+class Producto {
+  static contPro = 0;
+  constructor(nombre, precio) {
+    this._idproducto = ++Producto.contPro;
+    this._nombre = nombre;
+    this._precio = precio;
+  }
+
+  get _idproducto() {
+    return this._idproducto;
+  }
+
+  get nombre() {
+    return this._nombre;
+  }
+
+  set nombre(nombre) {
+    this._nombre = nombre;
+  }
+
+  get precio() {
+    return this._precio;
+  }
+
+  set precio(precio) {
+    this._precio = precio;
+  }
+
+  toString() {
+    return `${this._idPersona} ${this._nombre} ${this._precio}`;
+  }
+}
+
+class Orden {
+  static contOrd = 0;
+
+  static get MAX_PROD() {
+    return 5;
+  }
+
+  constructor() {
+    this._idOrden = ++Orden.contOrd;
+    this._productos = [];
+    this.contProAdd = 0;
+  }
 }
